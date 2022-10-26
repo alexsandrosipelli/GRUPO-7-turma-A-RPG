0e96dd46ff81863edf9a434a11c0aeeb6c0f70f9
Alexsandro Sipelli <alexsandrosipelli@outlook.com>
2022-10-26 17:19:30 -0300

    sera que vai mesmo

---
+++ JogoDeAdivinhar.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.jogodeadivinhar;
+
+/**
+ *
+ * @author alexs
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+public class JogoDeAdivinhar {
+
+    public static void main(String[] args) {
+        int num;
+        int a;
+        int t=0;
+        Scanner ler = new Scanner(System.in);
+        Random aleatorio = new Random (); 
+         a=aleatorio.nextInt(100);
+        do {  
+            
+            System.out.println(" digite um numero : "  );
+           num=ler.nextInt();
+        if(num == a ){System.out.println("“Parabéns, você ganhou o jogo!” ");}
+        if (num - a == 1 || num + 1 == a  ){System.out.println(" esta quente ");}
+        if (num<a  ) {System.out.println(" É maior ");}
+        
+        if (num>a  ) {System.out.println(" É menor ");}
+        t++;
+            }while((t!=5) && ( num !=a ));
+        if (num !=a){System.out.println("game over!!! ");}
+        
+        System.out.println("\n \n O numero era exatamente  " +  a );
+}
+}
